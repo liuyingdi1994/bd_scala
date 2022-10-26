b4a17611835cb4430b01385b69cace24d26736ab
liuyingdi <ydliu@cnic.cn>
2022-10-26 16:50:06 +0800

    Initial commit

---
+++ src/com/immoc/demo_01/Demo01.scala
@@ -0,0 +1,10 @@
+package com.immoc.demo_01
+
+object Demo01 {
+
+    def main(args: Array[String]): Unit = {
+        var number1 = 1
+        var number2 = 1.23
+        println(number1 + number2)
+    }
+}
